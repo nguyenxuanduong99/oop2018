612535b6900300f769744dee328faf41e45a33f6
nguyenxuanduong99 <37437106+nguyenxuanduong99@users.noreply.github.com>
2018-09-17 20:59:32 +0700

    Update StudentManagement.java

--- src/week1/StudentManagement.java
+++ src/week1/StudentManagement.java
@@ -3,14 +3,16 @@ package week1;
 public class StudentManagement {
 
     // TODO: khai báo thuộc tính students là array chứa các đối tượng thuộc lớp Student (max. 100)
-
+    public Student[] students = new Student[100];
+    
     public boolean sameGroup(Student s1, Student s2) {
         // TODO:
-        return false; // xóa dòng này sau khi cài đặt
+        return s1.getGroup().equals(s2.getGroup()); 
     }
 
     void studentsByGroup() {
         // TODO:
+        System.out.println("Danh sach sinh vien lop INT22041:");
     }
 
     void removeStudent(String id) {
