35ee80a54608efc44cd98700e5112982bba5fc55
nguyenxuanduong99 <37437106+nguyenxuanduong99@users.noreply.github.com>
2018-09-18 21:35:49 +0700

    Update Student.java

--- src/week1/Student.java
+++ src/week1/Student.java
@@ -3,20 +3,51 @@ package week1;
 public class Student {
 
     // TODO: khai báo các thuộc tính cho Student
-    private string name;
-    private string id;
-    private string group;
-    private string email;
+    private String name;
+    private String id;
+    private String group;
+    private String email;
     // TODO: khai báo các phương thức getter, setter cho Student
-    public string getName(string name){return name;}       public void setName(){this.name=name;}
-    public string getId(string id){return id;}           public void setId(){this.id=id;}
-    public string getGroud(string group){return group;}     public void setGroud(){this.group=group;}
-    public string getEmail(string email){return email;}     public void setEmail(){this.email=email;}
-    /**
+   public String getName(){
+
+        return name;
+    }
+    public void setName(String tenSV){
+
+        this.name= tenSV;
+    }
+    public String getID(){
+
+        return id;
+    }
+    public void setID(String maSV){
+
+        this.id = maSV;
+    }
+    public String getGroup(){
+
+        return group;
+    }
+    public void setGroup(String tenGroup){
+
+        this.group = tenGroup;
+    }
+    public String getEmail(){
+
+        return email;
+    }
+    public void setEmail(String nEmail) {
+
+        this.email = nEmail;
+    }
      * Constructor 1
      */
     Student() {
         // TODO:
+        this.name = "Student";
+        this.id = "000";
+        this.group = "INT22041";
+        this.email = "uet@vnu.edu.vn";
     }
 
     /**
@@ -27,6 +58,10 @@ public class Student {
      */
     Student(String n, String sid, String em) {
         // TODO:
+        this.name = n;
+        this.id = sid;
+        this.group = "INT22041";
+        this.email = em;
     }
 
     /**
@@ -35,10 +70,15 @@ public class Student {
      */
     Student(Student s) {
         // TODO:
+        this.name = s.getName();
+        this.id = s.getID();
+        this.group = s.getGroup();
+        this.email = s.getEmail();
     }
 
     String getInfo() {
         // TODO:
-        return null; // xóa dòng này sau khi cài đặt
+        return this.getName()+"\n"+this.getID()+"\n"+this.getGroup()+"\n"+this.getEmail();
+
     }
 }
