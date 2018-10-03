af93ed97a90b537e070cf3fa9ac28726e24e87bf
nguyenxuanduong99 <37437106+nguyenxuanduong99@users.noreply.github.com>
2018-10-03 13:27:42 +0700

    Update Week3Test.java

--- test/week3/Week3Test.java
+++ test/week3/Week3Test.java
@@ -7,27 +7,27 @@ import org.junit.Test;
 public class Week3Test {
 
     // TODO: Viết 5 testcase cho phương thức max()
-    Assert.assertEquals(5 , Week3.max(5 , 3));
-    Assert.assertEquals(10, Week3.max(2 ,10));
-    Assert.assertEquals(122, Week3.max(122 ,34))
-    Assert.assertEquals(99, Week3.max(2 ,99));
-    Assert.assertEquals(345, Week3.max(215 ,345));
+    Assert.assertEquals(10 , Week3.max(10 , 3));
+    Assert.assertEquals(2, Week3.max(2 ,10));
+    Assert.assertEquals(12, Week3.max(22 ,34))
+    Assert.assertEquals(9, Week3.max(2 ,99));
+    Assert.assertEquals(35, Week3.max(25 ,35));
     // TODO: Viết 5 testcase cho phương thức minOfArray()
-    int arr_1[] = new int[] {4 , 3 ,5 };
+    int arr_1[] = new int[] {10 , 3 ,8 };
         Assert.assertEquals(3 ,Week3.minOfArray(arr_1));
-    int arr_2[] = new int[] {6 , 3 , 7 , 0 , 12 , 6};
+    int arr_2[] = new int[] {9 , 3 , 5 , 0 , 10 , 6};
         Assert.assertEquals(0 , Week3.minOfArray(arr_2));
-    int arr_3[] = new int[] {122 , 99 , 67 , 40 , 12 , 5};
+    int arr_3[] = new int[] {22 , 9 , 6 , 480 , 126 , 5};
         Assert.assertEquals(5 ,Week3.minOfArray(arr_3));
-    int arr_4[] = new int[] {1 , 3 , 5 , 0 , 12 };
+    int arr_4[] = new int[] {18 , 39 , 5 , 0 , 2 };
         Assert.assertEquals(0 , Week3.minOfArray(arr_4));
-    int arr_5[] = new int[] {6 , 13 , 8 , 10 , 2 , 7};
+    int arr_5[] = new int[] {64 , 3 , 8 , 9 , 2 , 7};
         Assert.assertEquals(2 , Week3.minOfArray(arr_5));
 
     // TODO: Viết 5 testcase cho phương thức calculateBMI()
-    Assert.assertEquals("Bình thường" , Week3.calculateBMI(52 , 1.65));
-    Assert.assertEquals("Thiếu cân" , Week3.calculateBMI(40 , 1.65));
-    Assert.assertEquals("Thừa cân" , Week3.calculateBMI(67 , 1.7));
-    Assert.assertEquals("Béo phì" , Week3.calculateBMI(100 , 1.7));
-    Assert.assertEquals("Bình thường" , Week3.calculateBMI(55 , 1.68));
+    Assert.assertEquals("Bình thường" , Week3.calculateBMI(62 , 1.65));
+    Assert.assertEquals("Thiếu cân" , Week3.calculateBMI(42 , 1.65));
+    Assert.assertEquals("Thừa cân" , Week3.calculateBMI(77 , 1.7));
+    Assert.assertEquals("Béo phì" , Week3.calculateBMI(98 , 1.7));
+    Assert.assertEquals("Bình thường" , Week3.calculateBMI(65 , 1.68));
 }
