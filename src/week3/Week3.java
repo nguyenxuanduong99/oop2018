7a24c4b7a09936bcba5cdec75d9a601c985bdfb7
nguyenxuanduong99 <37437106+nguyenxuanduong99@users.noreply.github.com>
2018-10-03 13:12:24 +0700

    Update Week3.java

--- src/week3/Week3.java
+++ src/week3/Week3.java
@@ -12,8 +12,9 @@ public class Week3 {
     public static int minOfArray(int[] array) {
         // TODO: Tìm giá trị nhỏ nhất của của một mảng số nguyên (kích thước mảng <= 100 phần tử)
         int i;
+        int n = array.length;
         int lonnhat=array[0];
-        for(i=1;i<=100;i++)
+        for(i=1;i<n;i++)
         {
             if(array[i]>lonnhat) lonnhat=array[i];
         }
@@ -28,6 +29,18 @@ public class Week3 {
      */
     public static String calculateBMI(double weight, double height) {
         // TODO: Viết chương trình tính chỉ số BMI và in ra kết quả đánh giá
-        return null;
+        double BMI = weight / (height*height);
+        if(BMI < 18.5){
+            return "Thiếu cân";
+        }
+        else if(BMI >= 18.5 && BMI <= 22.99){
+            return "Bình thường";
+        }
+        else if(BMI >=23 && BMI <= 24.99){
+            return "Thừa cân";
+        }
+        else {
+            return "Béo phì";
+        }
     }
 }
