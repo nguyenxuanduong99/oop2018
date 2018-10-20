2d61e1b2a4a49870c79b53df52f2a15c84a42e26
nguyenxuanduong99 <37437106+nguyenxuanduong99@users.noreply.github.com>
2018-10-20 21:44:58 +0700

    Update Shape.java

--- src/week5_6/Shape.java
+++ src/week5_6/Shape.java
@@ -1,5 +1,35 @@
 package week5_6;
 
-public class Shape {
+import java.awt.*;
 
+/**
+ * class tạo màu sắc và cách di chuyển của hình
+ */
+public class Shape {
+    public Color color;
+    public Point point_temp = new Point();
+    public Point point = new Point();
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    /**
+     * phương thúc di chuyển hình
+     */
+    public void move(){
+        if(point_temp.getX() < 0|| point_temp.getX() > 500){
+            point.setX(-point.getX());
+        }
+        if(point_temp.getY() < 0|| point_temp.getY() > 500){
+            point.setY(-point.getY());
+        }
+        point_temp.setX(point_temp.getX() + point.getX());
+        point_temp.setY(point_temp.getY()+point.getY());
+    }
 }
+
