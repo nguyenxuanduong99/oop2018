4d76a35db6e8d392b2f20960b7d2a029f6b6422e
nguyenxuanduong99 <37437106+nguyenxuanduong99@users.noreply.github.com>
2018-10-20 21:47:22 +0700

    Update Diagram.java

--- src/week5_6/Diagram.java
+++ src/week5_6/Diagram.java
@@ -1,4 +1,32 @@
 package week5_6;
 
-public class Diagram {
+import javax.swing.*;
+import java.util.ArrayList;
+
+public class Diagram extends Layer {
+    public void removeCircle(){
+        for (int i=0;i<layer.size();i++){
+            if(layer.get(i) instanceof Circle)
+            {
+                layer.remove(i);
+                i--;
+            }
+        }
+
+    }
+    public static void main(String[] abc){
+        JFrame jFrame = new JFrame();
+        Layer newLayer = new Layer();
+        Diagram d = new Diagram();
+        System.out.println("số hình là: " + newLayer.layer.size());
+        newLayer.removeTriangle();
+        newLayer.removeCircle();
+        System.out.println("số hình sau khi xóa la: " + newLayer.layer.size());
+
+        jFrame.add(newLayer);
+        jFrame.setVisible(true);
+        jFrame.setSize(600 , 580);
+        jFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    }
 }
+
