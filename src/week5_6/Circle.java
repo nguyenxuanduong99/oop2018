7a5f1964128f1ffb8bdc9d2292e26caa1b94de8a
nguyenxuanduong99 <37437106+nguyenxuanduong99@users.noreply.github.com>
2018-10-20 21:46:55 +0700

    Update Circle.java

--- src/week5_6/Circle.java
+++ src/week5_6/Circle.java
@@ -1,4 +1,37 @@
 package week5_6;
 
-public class Circle {
+import javax.swing.*;
+import java.awt.*;
+import java.util.Random;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+//hinh tron
+
+public class Circle extends Shape{
+    private int radius;
+    public Circle(){
+        Random random = new Random();
+        this.radius = random.nextInt(80);
+        this.point_temp.setX(random.nextInt(30));
+        this.point_temp.setY(random.nextInt(30));
+        this.point.setX(random.nextInt(6));
+        this.point.setY(random.nextInt(6));
+        this.color = new Color(random.nextInt(255) , random.nextInt(255) , random.nextInt(255));
+
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+    @Override
+    public void move(){
+        super.move();
+    }
 }
+
+
