619cbf2f0c57d5e2989f39c3a82644151d60d201
nguyenxuanduong99 <nguyenduonguet62@gmail.com>
2018-10-20 21:59:53 +0700

    duong

--- src/week4/task1/main.java
+++ src/week4/task1/main.java
@@ -1,20 +1,20 @@
-package week4.task1;
-
-/**
- * Created by CCNE on 10/10/2018.
- */
-public class main {
-    public static void main(String[] aba) {
-        HoaQua hq = new HoaQua();
-        Tao t = new Tao();
-        Cam c= new Cam();
-        Camcaophong ccp= new Camcaophong();
-        Camsanh cs=new Camsanh();
-        hq.setTen("Qua");
-        hq.setCan("20kilo");
-        t.setVi("Ngot");
-        c.setNgaynhap("22/10");
-        ccp.setMau("Da cam");
-        cs.setSoLuong("1500");
-    }
-}
+//package week4.task1;
+//
+///**
+// * Created by CCNE on 10/10/2018.
+// */
+//public class main {
+//    public static void main(String[] aba) {
+//        HoaQua hq = new HoaQua();
+//        Tao t = new Tao();
+//        Cam c= new Cam();
+//        Camcaophong ccp= new Camcaophong();
+//        Camsanh cs=new Camsanh();
+//        hq.setTen("Qua");
+//        hq.setCan("20kilo");
+//        t.setVi("Ngot");
+//        c.setNgaynhap("22/10");
+//        ccp.setMau("Da cam");
+//        cs.setSoLuong("1500");
+//    }
+//}
---
+++ src/week5_6/Point.java
@@ -0,0 +1,25 @@
+package week5_6;
+
+/**
+ * class tạo tọa độ cho hình
+ */
+public class Point {
+    private int x ;
+    private int y;
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
