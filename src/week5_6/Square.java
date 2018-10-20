41cac6773a83e362e4268bb5e5360e6580c56163
nguyenxuanduong99 <37437106+nguyenxuanduong99@users.noreply.github.com>
2018-10-20 21:44:24 +0700

    Update Square.java

--- src/week5_6/Square.java
+++ src/week5_6/Square.java
@@ -1,4 +1,34 @@
-package week5_6;
+// class word
+import java.util.Scanner;
 
-public class Square {
+public class Word
+{
+	public String word;
+	public String mean;
+	public String type;
+	public int STT;
+
+	// ham tao
+	public Word()
+	{
+
+	}
+
+	// ham tao co tham so
+	public Word(int STT,String word, String mean, String type)
+	{
+		this.STT = STT;
+		this.word = word;
+		this.mean= mean;
+		this.type = type;
+
+	}
+
+	public void displayWord()
+	{
+
+		System.out.printf("\n|%-20s|%-38s|%-17s|",word,mean,type);
+		System.out.printf("\n|-----------------------------------------------------------------------------|");
+
+	}
 }
