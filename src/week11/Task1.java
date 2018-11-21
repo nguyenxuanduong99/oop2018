7a52edc57d2802af424f59c3df5a51d6e6165190
nguyenxuanduong99 <nguyenduonguet62@gmail.com>
2018-11-21 14:20:29 +0700

    duongtv99

--- src/week11/Task1.java
+++ src/week11/Task1.java
@@ -1,4 +1,39 @@
 package week11;
 
 public class Task1 {
+    public<T>void swap(T[]arr,int i,int j){
+        if(i!=j){
+            T tmp =arr[i];
+            arr[i]=arr[j];
+            arr[j]=tmp;
+        }
+    }
+    public<T extends Comparable<T>> void sap_xep(T[]arr){
+        int n=arr.length;
+        for(int i=0;i<n-1;i++){
+            int min=i;
+            for(int j=i+1;j<n;j++) {
+                if (arr[j].compareTo(arr[min]) <= 0) {
+                    min = j;
+                }
+            }
+            swap(arr,i,min);
+        }
+        for (int i=0;i<n;i++){
+            System.out.println(arr[i]);
+        }
+    }
+
+    public static void main(String[] abc){
+        Task1 n=new Task1();
+        Byte[] B={1,0};
+        Integer[] I ={3,9,7,1,2,6};
+        Double[] F={3.5,9.6,6.9,1.2,4.3,3.4};
+        Character[] C={'a','f','b','m','c','k'};
+        n.sap_xep(B);
+        n.sap_xep(I);
+        n.sap_xep(F);
+        n.sap_xep(C);
+
+    }
 }
--- src/week11/Task2.java
+++ src/week11/Task2.java
@@ -1,4 +1,35 @@
 package week11;
 
 public class Task2 {
+    public<T>void swap(T[]arr,int i,int j){
+        if(i!=j){
+            T tmp =arr[i];
+            arr[i]=arr[j];
+            arr[j]=tmp;
+        }
+    }
+    public<T extends Comparable<T>> void lon_nhat(T[]arr){
+        int n=arr.length;
+        for(int i=0;i<n-1;i++){
+            int min=i;
+            for(int j=i+1;j<n;j++) {
+                if (arr[j].compareTo(arr[min]) <= 0) {
+                    min = j;
+                }
+            }
+            swap(arr,i,min);
+        }
+        System.out.println(arr[n-1]);
+    }
+    public static void main(String[] abc){
+        Task2 n=new Task2();
+        Byte[] B={1,0};
+        Integer[] I ={3,9,7,1,2,6};
+        Double[] F={3.5,9.6,6.9,1.2,4.3,3.4};
+        Character[] C={'a','f','b','m','c','k'};
+        n.lon_nhat(B);
+        n.lon_nhat(I);
+        n.lon_nhat(F);
+        n.lon_nhat(C);
+    }
 }
